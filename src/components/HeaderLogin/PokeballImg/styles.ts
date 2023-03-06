a3355f2c4aa11c665575b94de3dff692f7dee04c
toledomg <toledomg@yahoo.com.br>
2023-03-06 17:29:54 -0300

    :lipstick: feat: estilização @media-responsive css do formulario login

--- src/components/HeaderLogin/PokeballImg/styles.ts
+++ src/components/HeaderLogin/PokeballImg/styles.ts
@@ -3,36 +3,36 @@ import styled from 'styled-components';
 export const StyledPokeballDiv = styled.div`
   position: fixed;
 
-  top: -20vw;
+  top: -10vw;
   right: -10vh;
   opacity: 0.2;
   transform: rotate(-10deg);
 
   img {
-    width: 350px;
+    width: 400px;
     height: auto;
   }
 
   @media (min-width: 374px) {
     img {
-      width: 350px;
+      width: 400px;
       height: auto;
     }
   }
 
   @media screen and (min-width: 425px) and (max-width: 759px) {
     img {
-      width: 375px;
+      width: 400px;
       height: auto;
     }
   }
 
   @media (min-width: 760px) and (max-width: 1019px) {
-    top: -15vw;
+    top: -10vw;
     right: -5vh;
 
     img {
-      width: 425px;
+      width: 500px;
       height: auto;
     }
   }
