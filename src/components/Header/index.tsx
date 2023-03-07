51022f45be261f6102262bf8893ae593e0859ae7
toledomg <toledomg@yahoo.com.br>
2023-03-07 07:31:54 -0300

    :sparkles: feat: implementada função logout no botão da dashboard

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,29 @@
-import { HeaderContainer, Logo, YellowLine } from './style'
-import logo from '../../assets/kenzieDex2.png'
-import logout from '../../assets/logout icon.png'
-import profile from '../../assets/profile icon.png'
-import yellowLine from '../../assets/yellowLine.png'
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import { useContext } from 'react';
+import { HeaderContainer, Logo, YellowLine } from './style';
+import logo from '../../assets/kenzieDex2.png';
+import logout from '../../assets/logout icon.png';
+import profile from '../../assets/profile icon.png';
+import yellowLine from '../../assets/yellowLine.png';
+import { UserContext } from '../../providers/UserContext';
 
-const Header = () => (
- <>
-    <HeaderContainer>
-      <Logo src={logo} alt='Logo' />
+const Header = () => {
+  const { userLogout } = useContext(UserContext);
 
-      <div>
-      <img src={profile} alt='profile icon' />
-      <img src={logout} alt='logout icon' />
-      </div>
+  return (
+    <>
+      <HeaderContainer>
+        <Logo src={logo} alt='Logo' />
 
-    </HeaderContainer>
+        <div>
+          <img src={profile} alt='profile icon' />
+          <img src={logout} alt='logout icon' onClick={() => userLogout()} />
+        </div>
+      </HeaderContainer>
+      <YellowLine src={yellowLine} alt='yellow line' />
+    </>
+  );
+};
 
-    <YellowLine src={yellowLine} alt='yellow line'/>
-   </>
-)
-
-export default Header
+export default Header;
--- src/providers/PokemonContext.tsx
+++ src/providers/PokemonContext.tsx
@@ -1,48 +1,50 @@
-import {createContext, useEffect, useState} from 'react'
-import { api, apiList } from '../services/api';
+import { createContext, useEffect, useState } from 'react';
+import { api } from '../services/api';
 
 export interface iPokemon {
   name: string;
   url: string;
 }
 
-export interface iType{
+export interface iType {
   name: string;
 }
 
-interface iPokemonContext{
+interface iPokemonContext {
   pokemonList: iPokemon[];
   setPokemonList: React.Dispatch<React.SetStateAction<iPokemon[]>>;
   pokemonTypes: iType[];
   setPokemonTypes: React.Dispatch<React.SetStateAction<iType[]>>;
 }
 
-interface iPokemonContextProps{
+interface iPokemonContextProps {
   children: React.ReactNode;
 }
 
-export const PokemonContext = createContext({} as iPokemonContext)
+export const PokemonContext = createContext({} as iPokemonContext);
 
-export const PokemonProvider = ({children}: iPokemonContextProps) => {
-  const [pokemonList, setPokemonList ] = useState<iPokemon[]>([])
-  const [pokemonTypes, setPokemonTypes] = useState<iType[]>([])
+export const PokemonProvider = ({ children }: iPokemonContextProps) => {
+  const [pokemonList, setPokemonList] = useState<iPokemon[]>([]);
+  const [pokemonTypes, setPokemonTypes] = useState<iType[]>([]);
 
   useEffect(() => {
     const loadPokemons = async () => {
       try {
-        const response = await apiList.get('/pokemon')
-        setPokemonList(response.data.results)
-        console.log(response.data.results)
+        const response = await api.get('/pokemon');
+        setPokemonList(response.data.results);
+        console.log(response.data.results);
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
-    loadPokemons()
-  }, [])
+    };
+    loadPokemons();
+  }, []);
 
   return (
-    <PokemonContext.Provider value={{pokemonList, setPokemonList, pokemonTypes, setPokemonTypes}}>
+    <PokemonContext.Provider
+      value={{ pokemonList, setPokemonList, pokemonTypes, setPokemonTypes }}
+    >
       {children}
     </PokemonContext.Provider>
-  )
-}
+  );
+};
