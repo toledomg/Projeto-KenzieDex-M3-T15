b360604e1175988f856c5b65894a528e72c0dcb3
toledomg <toledomg@yahoo.com.br>
2023-03-13 20:47:42 -0300

    :sparkles: feat: implementado animação entre paginas, usando o frame-motion

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import { StyledLoginPage } from './style';
 import { StyledContainer, StyledGridBox } from '../../styles/grid';
 import { StyledTitle } from '../../styles/typography';
@@ -6,18 +7,25 @@ import LoginForm from '../../components/Form/LoginForm';
 import PokeballImg from '../../components/HeaderLogin/PokeballImg';
 
 const LoginPage = () => (
-  <StyledLoginPage>
-    <Header />
-    <StyledContainer>
-      <StyledGridBox>
-        <StyledTitle tag='h2' $fontSize='three'>
-          Login
-        </StyledTitle>
-        <LoginForm />
-      </StyledGridBox>
-    </StyledContainer>
-    <PokeballImg />
-  </StyledLoginPage>
+  <motion.div
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ opacity: 1 }}
+  >
+    <StyledLoginPage>
+      <Header />
+      <StyledContainer>
+        <StyledGridBox>
+          <StyledTitle tag='h2' $fontSize='three'>
+            Login
+          </StyledTitle>
+          <LoginForm />
+        </StyledGridBox>
+      </StyledContainer>
+      <PokeballImg />
+    </StyledLoginPage>
+  </motion.div>
 );
 
 export default LoginPage;
