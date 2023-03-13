61662ce9d78e104b63d848d917340463f164d30d
toledomg <toledomg@yahoo.com.br>
2023-03-13 18:16:38 -0300

    :bug: fix: corrigido tipagem função theWinner na battleTeam

--- src/components/BattleArena/BattleOponent/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BattleOponent = () => {
-  console.log('teste');
-
-  return <div>BattleOponent</div>;
-};
-
-export default BattleOponent;
--- src/components/BattleArena/BattleTeam/index.tsx
+++ src/components/BattleArena/BattleTeam/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
 /* eslint-disable jsx-a11y/heading-has-content */
 import { useContext, useEffect, useState } from 'react';
 import { PokemonContext } from '../../../providers/PokemonContext';
