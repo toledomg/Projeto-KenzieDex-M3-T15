d1fd7d0b8d4327fda9715e40523f93a243784863
toledomg <toledomg@yahoo.com.br>
2023-03-13 11:02:11 -0300

    :lipstick: feat: css input:focus

--- src/components/SearchInput/style.tsx
+++ src/components/SearchInput/style.tsx
@@ -2,11 +2,14 @@ import styled from 'styled-components';
 
 export const SearchInputContainer = styled.div`
   width: 280px;
-  height: 25px;
+  height: 32px;
 
   background-color: ${({ theme }) => theme.colors.gray2};
 
-  border: 1px solid ${({ theme }) => theme.colors.secondary.default};
+  :focus-within {
+    border: 1px solid #ffcc0160;
+  }
+  /* border: 1px solid ${({ theme }) => theme.colors.secondary.default}; */
   border-radius: 4px;
 
   color: ${({ theme }) => theme.colors.gray0};
@@ -23,7 +26,7 @@ export const SearchInputContainer = styled.div`
 
   gap: 0.5rem;
 
-  padding: 0 5px;
+  padding: 2px 5px;
 
   > input {
     background-color: transparent;
@@ -37,16 +40,15 @@ export const SearchInputContainer = styled.div`
   }
 
   > button {
-        background-color: transparent;
+    background-color: transparent;
 
-        color: ${({theme}) => theme.colors.primary.default};
+    color: ${({ theme }) => theme.colors.primary.default};
 
-        margin-right: 5px;
-      }
-      
-  @media(min-width: 768px){
-      width: 436px;
-      height: 39px;
+    margin-right: 5px;
+  }
 
+  @media (min-width: 768px) {
+    width: 436px;
+    height: 39px;
   }
 `;
